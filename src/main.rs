7ac0b80305a1b9f77e36466427621ee521b07d78
Heng GAO <heng.gao.us@gmail.com>
2024-10-09 23:36:10 -0500

    add stage: REPL

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn run_command(input: &str) {
+    println!("{}: command not found", input.trim())
+}
+
 fn main() {
     // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    loop {
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        run_command(input.as_ref());
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 
     // strip line breaker from input
-    if let Some(input) = input.strip_suffix("\n") {
-        println!("{}: command not found", input)
-    };
+    // Improvement
+    // if let Some(input) = input.strip_suffix("\n") {
+    //     println!("{}: command not found", input)
+    // };
 }
